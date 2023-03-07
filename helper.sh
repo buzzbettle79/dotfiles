b580162484c84574232c8fb395e9d3c8696c0e18
BuzzBettle79 <buzzbettle79@proton.me>
2023-03-07 23:47:24 +1000

    source cargo env after running rustup

--- helper.sh
+++ helper.sh
@@ -10,7 +10,7 @@ echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >> ~/.z
 
 # Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-
+source "$HOME/.cargo/env"
 # Install LunarVim
 LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
 
